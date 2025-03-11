b3b4bfe0ec276c83b6a67dcc1e39cd0c613ca6cc
Clement Maroy <clement.maroy@gmail.com>
2025-03-11 17:30:10 +0100

    Update database password placeholder in RDS configuration

--- database.tf
+++ database.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "db" {
   instance_class         = "db.t3.micro"
   identifier             = "mydb"
   username               = "admin"
-  password               = "cHpM57*B9%PSC!ZOVnA2"
+  password               = "CHANGEME"
   parameter_group_name   = "default.mariadb10.6"
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
